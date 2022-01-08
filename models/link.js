3b2f8fda92d7097a7440ca545a5f91dc227cd631
arahiko-ayami <zinang7@gmail.com>
2022-01-08 19:31:52 +0700

    add: link model

---
+++ models/link.js
@@ -0,0 +1,14 @@
+const mongoose = require("../config/mongo");
+
+const CodeSchema = new mongoose.Schema({
+  _id: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("code", CodeSchema);
